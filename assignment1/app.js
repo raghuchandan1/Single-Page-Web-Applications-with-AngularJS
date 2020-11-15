f502be40ded52a0e6b5a561a8db2e295e972505e
Raghu Chandan <33779280+Raghu-Chandan@users.noreply.github.com>
2020-11-15 13:54:27 +0530

    Finished graded and ungraded tasks

--- assignment1/app.js
+++ assignment1/app.js
@@ -1,7 +1,38 @@
-(function() {
-'use strict';
+(function () {
+    'use strict';
 
-angular.module('LunchCheck', [])
-.controller();
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+        $scope.lunchMenu = "";
+        $scope.message = "";
+        $scope.msgColor = "black";
+        $scope.borderColor;
+
+        $scope.checkIfTooMuch = function () {
+            if ($scope.lunchMenu == "") {
+                $scope.borderColor = "red";
+                $scope.msgColor = "red";
+                $scope.message = "Please enter data first";
+            }
+
+            else {
+                $scope.borderColor = "green";
+                $scope.msgColor = "green";
+                var lunchItems = $scope.lunchMenu.split(',');
+                // console.log(lunchItems);
+                // console.log(lunchItems.length);
+                if (lunchItems.length <= 3) {
+                    $scope.message = "Enjoy!"
+                }
+                else {
+                    $scope.message = "Too much!"
+                }
+                // console.log($scope.message);
+            }
+        };
+    }
 
 })();
